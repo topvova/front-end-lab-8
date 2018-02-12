a8754a500a0bc5f6bbcc4b3e8acc16187f511341
topvova <topvova@gmail.com>
2018-02-13 00:16:53 +0200

    Add task two

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,1 +1,33 @@
-// TODO task 2
+const UAHinEUR = 33.2090;
+const UAHinUSD = 27.0585;
+
+let currency = {};
+let i = 1;
+
+do {
+    amount = +prompt('Enter amount ' + i + ', please:', '0');
+
+    if (isNaN(amount)) {
+        alert('Enter a number, please');
+        continue;
+    } else if (amount < 0) {
+        alert('Enter a positive number, please');
+        continue;
+    }
+
+    currency[i] = amount;
+    i++;
+} while (i <= 2);
+
+function EURintoUAH(amount) {
+    const result = (UAHinEUR * amount).toFixed(0);
+    return amount + ' euros are equal ' + result + ' UAH, ';
+}
+
+function USDintoUAH(amount) {
+    const result = (UAHinUSD * amount).toFixed(0);
+    return amount + ' dollars are equal ' + result + ' UAH, ';
+}
+
+console.log(EURintoUAH(currency[1]) + USDintoUAH(currency[2]) +
+    'one euro is equal ' + (UAHinEUR / UAHinUSD).toFixed(4) + ' dollars.');
